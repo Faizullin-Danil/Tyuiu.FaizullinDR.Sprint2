afdc669157f67868ffc019380cbf308be031ca88
TyuiuFaizullinDR@outlook.com <toxen2005@mail.ru>
2023-10-03 13:08:51 +0500

    Task1

---
+++ Tyuiu.FaizullinDR.Sprint2.Task1.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.FaizullinDR.Sprint2.Task1.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.FaizullinDR.Sprint2.Task1.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c75552b5-6930-4f45-9293-20dde6ad8b25")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.FaizullinDR.Sprint2.Task1.V14/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.FaizullinDR.Sprint2.Task1.V14.Lib;
+
+namespace Tyuiu.FaizullinDR.Sprint2.Task1.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int a = 185;
+            int b = 216;
+            int c = 174;
+            int d = 917;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a,b,c,d);
+
+            Console.Title = "Спринт #2 | Выполнил: Файзуллин Д. Р. | ИИПБ-23-3";
+            //Длина строки 75 символов
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                            *");
+            Console.WriteLine("* Тема: Операции сравнения                                                             *");
+            Console.WriteLine("* Задание #1                                                                           *");
+            Console.WriteLine("* Вариант #14                                                                          *");
+            Console.WriteLine("* Выполнил: Файзуллин Данил Рамилевич | ИИПБ-23-3                                      *");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                             *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,                      *");
+            Console.WriteLine("* последовательность можно чередовать, но использовать один раз в выражении)           *");
+            Console.WriteLine("* и логических операций (|, &, ||, &&, !, ^, последовательность операций не должна     *");
+            Console.WriteLine("* нарушаться), а также арифметических выражений, которая вернет логическую             *");
+            Console.WriteLine("* последовательность(массив): (False, True, True, True, True, False),                  *");
+            Console.WriteLine("* при a = 185, b = 216, c = 174, d = 917                                               *");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                     *");
+            Console.WriteLine("****************************************************************************************");
+
+            Console.WriteLine("A = " + a);
+            Console.WriteLine("B = " + b);
+            Console.WriteLine("C = " + c);
+            Console.WriteLine("D = " + d);
+
+
+
+
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                                            *");
+            Console.WriteLine("****************************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
