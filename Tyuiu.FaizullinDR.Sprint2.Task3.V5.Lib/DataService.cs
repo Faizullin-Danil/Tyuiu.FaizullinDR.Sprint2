0154be75466fb2297c5e060c1cf4b87e3f3d3ab1
TyuiuFaizullinDR@outlook.com <toxen2005@mail.ru>
2023-10-05 23:27:50 +0500

    Task3

---
+++ Tyuiu.FaizullinDR.Sprint2.Task3.V5.Lib/DataService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.FaizullinDR.Sprint2.Task3.V5.Lib
+{
+    public class DataService : ISprint2Task3V5
+    {
+        public double Calculate(double x)
+        {
+            double y = 0;
+            if (x>0)
+            {
+                y = x - Math.Pow(((x + 1) / (x - 1)), x);
+            }
+            else
+            {
+                if (x==0)
+                {
+                    y = (x * x - Math.Cos(x * x)) / (x * x - Math.Sin(x * x) + 12);
+                }
+                else
+                {
+                    if ((x>-9)&&(x<0))
+                    {
+                        y = Math.Pow(6 + (4 / (x * x)), x);
+                    }
+                    else
+                    {
+                        if (x<-9)
+                        {
+                            y = Math.Pow(x, 3) + 10 * x - (Math.Pow(x, 2) / Math.Pow(x, 4));
+                        }
+                    }
+                }
+                
+             
+            }
+
+
+            return Math.Round(y, 3);
+        }
+    }
+}
---
+++ Tyuiu.FaizullinDR.Sprint2.Task3.V5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.FaizullinDR.Sprint2.Task3.V5.Lib;
+
+namespace Tyuiu.FaizullinDR.Sprint2.Task3.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+
+            Console.Title = "Спринт #2 | Выполнил: Файзуллин Д. Р. | ИИПБ-23-3";
+            //Длина строки 75 символов
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                            *");
+            Console.WriteLine("* Тема: Операции сравнения                                                             *");
+            Console.WriteLine("* Задание #3                                                                           *");
+            Console.WriteLine("* Вариант #5                                                                           *");
+            Console.WriteLine("* Выполнил: Файзуллин Данил Рамилевич | ИИПБ-23-3                                      *");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                             *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с использованием  *");
+            Console.WriteLine("* вложенных оператор if-else где пользователь вводит значение переменной X с клавиатуры*");
+            Console.WriteLine("* Округлить полученное значение до трех знаков после запятой                           *");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                     *");
+            Console.WriteLine("****************************************************************************************");
+
+
+            Console.WriteLine("Введите значение X:");
+            int x = Convert.ToInt32(Console.ReadLine());
+         
+
+
+
+
+
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                                            *");
+            Console.WriteLine("****************************************************************************************");
+
+            double y = ds.Calculate(x);
+            Console.WriteLine(y);
+
+
+            Console.ReadLine();
+        }
+    }
+}
