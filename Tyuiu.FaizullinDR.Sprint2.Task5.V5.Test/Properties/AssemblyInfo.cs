384ada79f6183aa74a89ec19f80c836f49227489
TyuiuFaizullinDR@outlook.com <toxen2005@mail.ru>
2023-10-09 16:28:29 +0500

    Task5

---
+++ Tyuiu.FaizullinDR.Sprint2.Task5.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.FaizullinDR.Sprint2.Task5.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.FaizullinDR.Sprint2.Task5.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0ec84f3-9c46-41b4-a7ac-4bf6273c26db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.FaizullinDR.Sprint2.Task5.V5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.FaizullinDR.Sprint2.Task5.V5.Lib;
+
+namespace Tyuiu.FaizullinDR.Sprint2.Task5.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+
+            Console.Title = "Спринт #2 | Выполнил: Файзуллин Д. Р. | ИИПБ-23-3";
+            //Длина строки 75 символов
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                            *");
+            Console.WriteLine("* Тема: Операции сравнения                                                             *");
+            Console.WriteLine("* Задание #5                                                                           *");
+            Console.WriteLine("* Вариант #5                                                                           *");
+            Console.WriteLine("* Выполнил: Файзуллин Данил Рамилевич | ИИПБ-23-3                                      *");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                             *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch,                              *");
+            Console.WriteLine("* вычисляет требуемое значение и возвращает результат.                                 *");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                     *");
+            Console.WriteLine("****************************************************************************************");
+
+
+            Console.WriteLine("Введите номер карты:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+           
+
+
+
+
+
+
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                                            *");
+            Console.WriteLine("****************************************************************************************");
+
+            string res = ds.FindCardValue(x);
+            Console.WriteLine(res);
+
+
+            Console.ReadLine();
+        }
+    }
+}
