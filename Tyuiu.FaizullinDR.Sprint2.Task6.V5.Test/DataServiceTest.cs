24960c27e8bfd48f6e1b12af32845445cea9abd1
TyuiuFaizullinDR@outlook.com <toxen2005@mail.ru>
2023-10-11 17:02:59 +0500

    Task6

---
+++ Tyuiu.FaizullinDR.Sprint2.Task6.V5.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.FaizullinDR.Sprint2.Task6.V5.Lib;
+
+namespace Tyuiu.FaizullinDR.Sprint2.Task6.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 14;
+            string res = ds.FindCardValue(x);
+            string wait = "туз";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
