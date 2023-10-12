8205b821d3b974b9a58f711a2e4830c4622a3572
TyuiuFaizullinDR@outlook.com <toxen2005@mail.ru>
2023-10-12 16:02:41 +0500

    Task7

---
+++ Tyuiu.FaizullinDR.Sprint2.Task7.V5.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.FaizullinDR.Sprint2.Task7.V5.Lib
+{
+    public class DataService : ISprint2Task7V5
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if ((y<Math.Exp(x))&&(y < Math.Exp(-x))&&(y>x*x))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.FaizullinDR.Sprint2.Task7.V5/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.FaizullinDR.Sprint2.Task7.V5.Lib;
+
+namespace Tyuiu.FaizullinDR.Sprint2.Task7.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+
+            Console.Title = "Спринт #2 | Выполнил: Файзуллин Д. Р. | ИИПБ-23-3";
+            //Длина строки 75 символов
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                            *");
+            Console.WriteLine("* Тема: Операции сравнения                                                             *");
+            Console.WriteLine("* Задание #7                                                                           *");
+            Console.WriteLine("* Вариант #5                                                                           *");
+            Console.WriteLine("* Выполнил: Файзуллин Данил Рамилевич | ИИПБ-23-3                                      *");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                             *");
+            Console.WriteLine("* Написать программу на C#, которая запрашивает исходные данные (вещественные значения)*");
+            Console.WriteLine("* и вычисляет, находится ли точка с координатами X,Y в заштрихованной области.         *");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                     *");
+            Console.WriteLine("****************************************************************************************");
+
+
+            Console.WriteLine("Введите X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+
+
+
+
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                                            *");
+            Console.WriteLine("****************************************************************************************");
+
+            bool res = ds.CheckDotInShadedArea(x,y);
+            if (res)
+            {
+                Console.WriteLine("точка находится в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("точка не находится в заштрихованной области");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
